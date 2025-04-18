d18d1edb18fab6875c5978aa722806276711fb1a
Fpistolet <florian.pistolet@gmail.com>
2025-04-18 12:37:27 +0200

    fix: écoute sur 0.0.0.0 pour Render

--- app.py
+++ app.py
@@ -133,4 +133,7 @@ def etudiants():
     return render_template("etudiants.html", etudiants=etudiants)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
